11712dae2f0100969f6edb51e7bdbc4e656eea92
Emrecan <emrecanyasar8@outlook.com>
2022-04-10 17:34:58 +0300

    Add project files.

---
+++ Basics/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Basics.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace Basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+        public IActionResult Authenticate()
+        {
+            var granmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Bob"),
+                new Claim(ClaimTypes.Name,"BOB@fmail.com"),
+                new Claim("Grandma.Says","Very nice boy"),
+            };
+            var licenseClaims=new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Bob K foo"),
+                new Claim("DrivingLicense","A+"),
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(granmaClaims,"Grandma Identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Goverment");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity,licenseIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
